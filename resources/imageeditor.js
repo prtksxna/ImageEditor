d34c7415f551fdc87f6e262a67642c03239f0130
Prateek Saxena <prtksxna@gmail.com>
2015-11-29 16:37:11 +0530

    Rename files and implement undo and redo

---
+++ resources/imageeditor.js
@@ -0,0 +1,422 @@
+( function ( $, OO, ImageTool ) {
+
+var ImageEditor;
+
+/**
+ * @class ImageEditor
+ *
+ * ImageEditor is a user interface that allows making edits to
+ * images. It uses OO.ui.Toolbar and OO.ui.PanelLayout for the basic
+ * UI, and Caman for image editing.
+ *
+ *     var e = new ImageEditor( {
+ *         containerId: 'editor',
+ *         imagePath: 'cat.png'
+ *     } );
+ *     e.initialize();
+ *
+ * @cfg {string} containerId DOM ID of the containter in which the
+ * editor will be rendered.
+ * @cfg {string} imagePath Path of the image to load in the editor.
+ */
+ImageEditor = function ( config ) {
+
+	if ( config.containerId === undefined || config.imagePath === undefined ) {
+		throw new Error( 'All config not passed' );
+	}
+
+	// Setup container
+	this.$container = $( '#' + config.containerId );
+	this.$container
+		.addClass( 'mwe-imageeditor-editor' )
+		.append(
+			$( '<div>' )
+				.addClass( 'mwe-imageeditor-canvas-container' )
+				.append(
+					$( '<img>' )
+						.attr( 'src', config.imagePath )
+						.attr( 'id', 'mwe-imageeditor-image' )
+				)
+		);
+
+	// Editor
+	this.editor = new OO.ui.PanelLayout( {
+		framed: true,
+		padded: false
+	} );
+	this.$container.append( this.editor.$element );
+
+	// Toolbar
+	this.toolFactory = new OO.ui.ToolFactory();
+	this.toolGroupFactory = new OO.ui.ToolGroupFactory();
+	this.toolbar = new OO.ui.Toolbar( this.toolFactory, this.toolGroupFactory, {
+		actions: true
+	} );
+	this.editor.$element.append( this.toolbar.$element );
+
+	/**
+	 * @property {Array} toolbarGroups The groups config passed to the
+	 * [toolbar's
+	 * setup](https://doc.wikimedia.org/oojs-ui/master/js/#!/api/OO.ui.Toolbar-method-setup)
+	 * method.
+	 */
+	this.toolbarGroups = [
+		{
+			type: 'bar',
+			include: [ 'undo', 'redo' ]
+		},
+		{
+			type: 'bar',
+			include: [ 'rotateCounterClockwise', 'rotateClockwise' ]
+		},
+		{
+			type: 'bar',
+			include: [ 'flipVertical', 'flipHorizontal' ]
+		},
+		{
+			type: 'bar',
+			include: [ 'crop' ]
+		}
+	];
+
+	/**
+	 * @property {Object} tools Instances of ImageTools registered with the ImageEditor
+	 */
+	this.tools = {};
+
+	/**
+	 * @property {Array} actions Actions taken so far.
+	 */
+	this.actions = [];
+
+	/**
+	 * @property {number} currentAction Current action as an index
+	 * of the {@link #property-actions} array.
+	 */
+	this.currentAction = undefined;
+
+	/**
+	 * @property {boolean} isUndoable Is the editor undoable?
+	 */
+	this.isUndoable = false;
+
+	/**
+	 * @property {boolean} isRedoable Is the editor redoable?
+	 */
+	this.isRedoable = false;
+};
+
+OO.initClass( ImageEditor );
+
+/**
+ * Initializes the editor.
+ */
+ImageEditor.prototype.initialize = function () {
+	this.setupToolbar();
+
+	// TODO Stuff about the editor's state
+};
+
+/**
+ * Setups up the toolbar.
+ */
+ImageEditor.prototype.setupToolbar = function () {
+	this.registerCoreTools();
+	this.setupUndoRedo();
+	this.setupTools();
+
+	// Setup toolbar
+	this.toolbar.setup( this.getToolbarGroups() );
+
+	this.saveButton = new OO.ui.ButtonWidget( {
+		label: 'Save',
+		flags: [ 'progressive', 'primary' ]
+	} );
+
+	// Refresh the undo redo states
+	this.toolbar.emit( 'updateState' );
+
+	this.toolbar.$actions.append( this.saveButton.$element );
+};
+
+/**
+ * Setter method for {@link #property-toolbarGroups}.
+ *
+ * @param {Object} groups
+ * @return {Object}
+ */
+ImageEditor.prototype.setToolbarGroups = function ( groups ) {
+	this.toolbarGroups = groups;
+	return this.toolbarGroups;
+};
+
+/**
+ * Getter method for {@link #property-toolbarGroups}.
+ *
+ * @return {Object}
+ */
+ImageEditor.prototype.getToolbarGroups = function () {
+	return this.toolbarGroups;
+};
+
+/**
+ * Pushes to {@link #property-actions}, updates {@link
+ * #property-currentAction}, and calls {@link #updateUndoRedoState}.
+ *
+ * @param {string} name
+ * @param {Object} action
+ */
+ImageEditor.prototype.addAction = function ( name, action ) {
+	// The current action is being made over the latest action
+	if (
+		this.currentAction === undefined ||
+		this.currentAction === this.actions.length - 1
+	) {
+		this.actions.push( {
+			name: name,
+			action: action
+		} );
+
+		this.currentAction = this.actions.length - 1;
+	} else {
+		this.actions = this.actions.slice( 0, this.currentAction + 1 );
+		this.actions.push( {
+			name: name,
+			action: action
+		} );
+
+		this.currentAction = this.actions.length - 1;
+	}
+	this.updateUndoRedoState();
+};
+
+/**
+ * Updates the state of the undo and redo buttons based on
+ * {@link #property-currentAction}.
+ */
+ImageEditor.prototype.updateUndoRedoState = function () {
+	this.isUndoable = ( this.currentAction >= 0 );
+	this.isRedoable = ( this.currentAction !== this.actions.length - 1 );
+	this.toolbar.emit( 'updateState' );
+};
+
+/**
+ * Undos last action
+ */
+ImageEditor.prototype.undo = function () {
+	var lastAction = this.actions[ this.currentAction ];
+	this.tools[ lastAction.name ].undoAction( '#mwe-imageeditor-image', lastAction.action );
+	this.currentAction--;
+	this.updateUndoRedoState();
+};
+
+/**
+ * Redos last action
+ */
+ImageEditor.prototype.redo = function () {
+	var nextAction = this.actions[ this.currentAction + 1 ];
+	this.tools[ nextAction.name ].doAction( '#mwe-imageeditor-image', nextAction.action );
+	this.currentAction++;
+	this.updateUndoRedoState();
+};
+
+/**
+ * Sets up the undo and redo buttons in the toolbar
+ */
+ImageEditor.prototype.setupUndoRedo = function () {
+	var editor =  this;
+
+	// Undo
+	function UndoTool() {
+		UndoTool.super.apply( this, arguments );
+	}
+
+	OO.inheritClass( UndoTool, OO.ui.Tool );
+
+	UndoTool.static.name = 'undo';
+	UndoTool.static.icon = 'undo';
+	UndoTool.static.title = 'Undo';
+
+	UndoTool.prototype.onSelect = function () {
+		editor.undo();
+		this.setActive( false );
+	};
+
+	UndoTool.prototype.onUpdateState = function () {
+		if ( editor.isUndoable ) {
+			this.setDisabled( false );
+		} else {
+			this.setDisabled( true );
+		}
+		this.setActive( false );
+	};
+
+	this.toolFactory.register( UndoTool );
+
+	// Redo
+	function RedoTool() {
+		RedoTool.super.apply( this, arguments );
+	}
+
+	OO.inheritClass( RedoTool, OO.ui.Tool );
+
+	RedoTool.static.name = 'redo';
+	RedoTool.static.icon = 'redo';
+	RedoTool.static.title = 'Redo';
+
+	RedoTool.prototype.onSelect = function () {
+		editor.redo();
+		this.setActive( false );
+	};
+
+	RedoTool.prototype.onUpdateState = function () {
+		if ( editor.isRedoable ) {
+			this.setDisabled( false );
+		} else {
+			this.setDisabled( true );
+		}
+		this.setActive( false );
+	};
+
+	this.toolFactory.register( RedoTool );
+};
+
+/**
+ * Reads list of registered tools and sets them up with the toolbar.
+ */
+ImageEditor.prototype.setupTools = function () {
+	$.each( this.tools, function ( tool ) {
+		this.setupTool( this.tools[ tool ] );
+	}.bind( this ) );
+};
+
+/**
+ * Sets up an instance of ImageTool with the toolbar.
+ */
+ImageEditor.prototype.setupTool = function ( tool ) {
+	var self = this;
+
+	function Tool() {
+		Tool.super.apply( this, arguments );
+	}
+	OO.inheritClass( Tool, OO.ui.Tool );
+
+	Tool.static.name = tool.name;
+	Tool.static.icon = tool.icon;
+	Tool.static.title = tool.title;
+
+	Tool.prototype.onSelect = function () {
+		var action = tool.doAction( '#mwe-imageeditor-image' );
+		self.addAction( tool.name, action );
+
+		this.setActive( false );
+	};
+
+	Tool.prototype.onUpdateState = function () {
+		this.setActive( false );
+	};
+
+	this.toolFactory.register( Tool );
+};
+
+/**
+ * Register an ImageTool with the editor
+ */
+ImageEditor.prototype.registerTool = function ( tool ) {
+	this.tools[ tool.name ] = tool;
+};
+
+/**
+ * Instantiate and register core tools with the editor
+ */
+ImageEditor.prototype.registerCoreTools = function () {
+	var rotateCounterClockwise, rotateClockwise, flipVertical, flipHorizontal;
+
+	rotateCounterClockwise = new ImageTool(  {
+		name: 'rotateCounterClockwise',
+		icon: 'rotate-counter-clockwise',
+		title: 'Rotate counter clockwise' // TODO Localizable
+	} );
+	rotateCounterClockwise.doAction = function ( image ) {
+		Caman( image, function () {
+			this.rotate( -90 );
+			this.render();
+		} );
+
+		return {};
+	};
+	rotateCounterClockwise.undoAction = function ( image ) {
+		Caman( image, function () {
+			this.rotate( 90 );
+			this.render();
+		} );
+	};
+	this.registerTool( rotateCounterClockwise );
+
+	rotateClockwise = new ImageTool(  {
+		name: 'rotateClockwise',
+		icon: 'rotate-clockwise',
+		title: 'Rotate clockwise'
+	} );
+	rotateClockwise.doAction = function ( image ) {
+		Caman( image, function () {
+			this.rotate( 90 );
+			this.render();
+		} );
+
+		return {};
+	};
+	rotateClockwise.undoAction = function ( image ) {
+		Caman( image, function () {
+			this.rotate( -90 );
+			this.render();
+		} );
+	};
+	this.registerTool( rotateClockwise );
+
+	flipVertical = new ImageTool( {
+		name: 'flipVertical',
+		icon: 'flip-vertical',
+		title: 'Flip vertical'
+	} );
+	flipVertical.doAction = function ( image ) {
+		Caman( image, function () {
+			this.flip( 'y' );
+			this.render();
+		} );
+
+		return {};
+	};
+	flipVertical.undoAction = function ( image ) {
+		Caman( image, function () {
+			this.flip( 'y' );
+			this.render();
+		} );
+	};
+	this.registerTool( flipVertical );
+
+	flipHorizontal = new ImageTool( {
+		name: 'flipHorizontal',
+		icon: 'flip-horizontal',
+		title: 'Flip horizontal'
+	} );
+	flipHorizontal.doAction = function ( image ) {
+		Caman( image, function () {
+			this.flip( 'x' );
+			this.render();
+		} );
+
+		return {};
+	};
+	flipHorizontal.undoAction = function ( image ) {
+		Caman( image, function () {
+			this.flip( 'x' );
+			this.render();
+		} );
+	};
+	this.registerTool( flipHorizontal );
+};
+
+window.ImageEditor = ImageEditor;
+
+}( jQuery, OO, ImageTool ) );
---
+++ resources/imagetool.js
@@ -0,0 +1,56 @@
+( function ( $, OO ) {
+
+var ImageTool;
+
+/**
+ * @class ImageTool
+ *
+ * Tools used within ImageEditor
+ *
+ * @cfg {string} name
+ * @cfg {string} icon
+ * @cfg {string} title
+ */
+ImageTool = function ( config ) {
+
+	if (
+		config.name === undefined ||
+		config.icon === undefined ||
+		config.title === undefined
+	) {
+		throw new Error( 'All config not passed' );
+	}
+
+	this.name =  config.name;
+	this.icon = config.icon;
+	this.title = config.title;
+};
+
+OO.initClass( ImageTool );
+
+/**
+ * Does the action of the tool.
+ *
+ * @abstract
+ * @method doAction
+ * @param {string} image Selector to pass to Caman
+ * @return {Object} action Returns an action object to be saved by the
+ * editor. This action should have data required by the tool to undo
+ * its action.
+ */
+// TODO doAction will have to return a promise at some point, for interactive tools
+ImageTool.prototype.doAction = null;
+
+/**
+ * Does the action of the tool.
+ *
+ * @abstract
+ * @method undoAction
+ * @param {string} image Selector to pass to Caman
+ * @param {Object} action Object required by the tool to undo its action.
+ */
+ImageTool.prototype.undoAction = null;
+
+window.ImageTool = ImageTool;
+
+}( jQuery, OO ) );
